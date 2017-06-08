863b6bc89dab16539ba83e5122236e34d561e189
André Carvalho <asantostc@gmail.com>
2017-06-08 18:19:36 -0300

    decrease maxconcurrency for tests

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ go get github.com/tsuru/tsuru-client/...
 export TSURU_INTEGRATION_examplesdir="${GOPATH}/src/github.com/tsuru/platforms/examples"
 export TSURU_INTEGRATION_installerconfig=${finalconfigpath}
 export TSURU_INTEGRATION_nodeopts="iaas=dockermachine"
-export TSURU_INTEGRATION_maxconcurrency=4
+export TSURU_INTEGRATION_maxconcurrency=2
 export TSURU_INTEGRATION_verbose=1
 export TSURU_INTEGRATION_enabled=1
 
